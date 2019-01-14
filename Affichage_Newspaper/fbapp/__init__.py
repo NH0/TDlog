ccb4bc09172965a4237d573dcbb0db066447a95a
NH0 <85ca4663@opayq.com>
2019-01-14 15:54:45 +0100

    init_db now initializes all tables

--- Affichage_Newspaper/fbapp/__init__.py
+++ Affichage_Newspaper/fbapp/__init__.py
@@ -9,6 +9,8 @@ models.db.init_app(app)
 @app.cli.command('init_db')
 def init_db():
     models.init_db()
+    models.init_db_login()
+    models.init_db_votes()
 
 @app.cli.command('init_db_login')
 def init_db_login():
