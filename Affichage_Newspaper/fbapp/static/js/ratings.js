ce40acd22abc67831992e607231048b602ed5eb0
NH0 <85ca4663@opayq.com>
2019-02-15 22:56:31 +0100

    All bootstrap code now is correctly imported; notation now partly works

--- Affichage_Newspaper/fbapp/static/js/ratings.js
+++ Affichage_Newspaper/fbapp/static/js/ratings.js
@@ -1,5 +1,3 @@
 alert('helloOne')
-let button = $('#form-rate');
-button.on('click',function() {
-    $.get( '/rateArticle', {"note":$('#note').val(), "idA":$('#form-rate').attr('idA')} )
- });
+let button = $('#form-rate')
+button.on('click',function() { $.post( '/rateArticle', {"note":$('#note').val(), "idA":$('#form-rate').attr('idA')} ) });
